08cb1e64050ffacfa215cc79ce9b8aff1779bb06
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-13 23:17:08 +0800

    create reducer slice and store

---
+++ src/reducers/users.ts
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { User, UserSlice } from '../types/user'
+
+// Slice Reducer
+////////////////////////////////
+
+const initialState: UserSlice = {
+  user: {
+    login: '',
+    id: null,
+    node_id: '',
+    avatar_url: '',
+    gravatar_id: '',
+    url: '',
+    html_url: '',
+    followers_url: '',
+    following_url: '',
+    gists_url: '',
+    starred_url: '',
+    subscriptions_url: '',
+    organizations_url: '',
+    repos_url: '',
+    events_url: '',
+    received_events_url: '',
+    type: '',
+    site_admin: false,
+    name: '',
+    company: '',
+    blog: '',
+    location: '',
+    email: '',
+    hireable: '',
+    bio: '',
+    twitter_username: '',
+    public_repos: null,
+    public_gists: null,
+    followers: null,
+    following: null,
+    created_at: '',
+    updated_at: '',
+  },
+  isLoadingUser: false,
+  hasErrorUser: false,
+}
+
+export const loadUser = createAsyncThunk(
+  'user/getUser',
+  async (userName: string): Promise<User> => {
+    const data = await fetch(`https://api.github.com/users/${userName}`)
+    const json = await data.json()
+    return json
+  },
+)
+
+export const users = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(loadUser.fulfilled, (state, action) => {
+      state.user = action.payload
+    })
+  },
+})
+
+export const {} = users.actions
+export default users.reducer
---
+++ src/types/user.ts
@@ -0,0 +1,40 @@
+export interface User {
+  login: string
+  id: number | null
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
+  name: string
+  company: string
+  blog: string
+  location: string
+  email?: any
+  hireable?: any
+  bio: string
+  twitter_username?: any
+  public_repos: number | null
+  public_gists: number | null
+  followers: number | null
+  following: number | null
+  created_at: string
+  updated_at: string
+}
+
+export interface UserSlice {
+  user: User
+  isLoadingUser: boolean
+  hasErrorUser: boolean
+}
