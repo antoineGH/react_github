051a65a4a3ee073cf0f9f94fb117c6d9c05d8a9b
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-13 19:33:56 +0800

    create menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './App.css'
+import { Layout } from 'antd'
+import GitHubMenu from '@components/gitHubMenu/GitHubMenu'
 
 function App() {
-  return <div className="App"></div>
+  const { Header, Footer, Content } = Layout
+  return (
+    <div className="App">
+      <Layout>
+        <Header>{GitHubMenu}</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+  )
 }
 
 export default App
