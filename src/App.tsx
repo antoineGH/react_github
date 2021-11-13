69e1e10f4d15f72221a3d333cbf4b623a778233f
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-13 19:53:54 +0800

    GitHubMenu completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './App.css'
 import { Layout } from 'antd'
-import GitHubMenu from '@components/gitHubMenu/GitHubMenu'
+import GitHubMenu from './components/gitHubMenu/GitHubMenu'
 
 function App() {
-  const { Header, Footer, Content } = Layout
+  const { Content } = Layout
   return (
     <div className="App">
       <Layout>
-        <Header>{GitHubMenu}</Header>
+        <GitHubMenu />
         <Content>Content</Content>
-        <Footer>Footer</Footer>
       </Layout>
     </div>
   )
