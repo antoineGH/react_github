642e4dec473cb27e2d2a7aa322a19254ec9bf540
Antoine Ratat <antoine.ratat@gmail.com>
2021-11-13 22:24:14 +0800

    remove delete button

--- src/components/gitHubUserResult/GitHubUserResult.tsx
+++ src/components/gitHubUserResult/GitHubUserResult.tsx
@@ -1,45 +1,12 @@
-import {
-  Col,
-  Row,
-  Card,
-  Descriptions,
-  Avatar,
-  Image,
-  Typography,
-  Button,
-} from 'antd'
-import { CloseOutlined } from '@ant-design/icons'
+import { Col, Row, Card, Descriptions, Avatar, Image, Typography } from 'antd'
 
 const GitHubUserResult = (): JSX.Element => {
   const { Title } = Typography
-
-  const handleClose = (): void => {
-    console.log('Close')
-  }
 
   return (
     <Row className="row-margin-1rem">
       <Col>
         <Card bordered={false}>
-          <Row>
-            <Col
-              xs={{ offset: 22 }}
-              md={{ offset: 23 }}
-              style={{
-                width: '100%',
-                justifyContent: 'center',
-                display: 'flex',
-              }}
-            >
-              <Button
-                id="btn-delete"
-                type="text"
-                icon={<CloseOutlined />}
-                size="large"
-                onClick={handleClose}
-              />
-            </Col>
-          </Row>
           <Row className="row-margin-1rem">
             <Col className="user-avatar">
               <Avatar
